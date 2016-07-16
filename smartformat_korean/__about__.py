29233c2f079ac1cc05548833cc9a63263da321e3
Heungsub Lee <sub@subl.ee>
2016-07-16 18:38:43 +0900

    Bump version: 0.0.2

--- smartformat_korean/__about__.py
+++ smartformat_korean/__about__.py
@@ -3,7 +3,7 @@
    smartformat.ext.korean.__about__
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __license__ = 'BSD'
 __author__ = 'What! Studio'
 __maintainer__ = 'Heungsub Lee'
